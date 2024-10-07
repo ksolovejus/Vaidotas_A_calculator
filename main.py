17bf2b3b97f37bb9c653055d6aea1505e1108b78
ksolovejus <solovejuskarolis@gmail.com>
2024-10-07 14:35:32 +0300

    Pretty code

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-class Calculator:    
-    def __init__(self, symbol: str, number: float) -> None:        
-        self.symbol = symbol        
-        self.number = number    
-        def add(self) -> float:        ...    def sub(self) -> float:        ...    def div(self) -> float:        ...    def mul(self) -> float:        ...    def calculate(self) -> float:        ... 
+class Calculator:
+    def __init__(self, symbol: str, number: float) -> None:
+        self.symbol = symbol
+        self.number = number
+
+    def add(self) -> float:
+        ...
+
+    def sub(self) -> float:
+        ...
+
+    def div(self) -> float:
+        ...
+
+    def mul(self) -> float:
+        ...
+
+    def calculate(self) -> float:
+        ...
+        
